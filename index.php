d929348e612703bb61a9b50c9e6bec0bae014ea3
brandon6190 <brandon6190@outlook.com>
2019-11-21 16:30:03 -0500

    This is another test commit

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php get_header(); consoleLog('Hello there');?>
+<?php get_header(); ?>
 
 <div id="vseen">
   <a class="vseen-logo" href="#"><img src="<?= get_theme_file_uri('/images/VSEEN-LOGO-WHITE.png'); ?>"
