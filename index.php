91786d145aac3b52732272f03c67a515e1192cb5
brandon6190 <brandon6190@outlook.com>
2019-11-21 15:56:21 -0500

    Added hompage markup from template

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div id="vseen">
+  <a class="vseen-logo" href="#"><img src="<?= get_theme_file_uri('/images/VSEEN-LOGO-WHITE.png'); ?>"
+      alt="VSEEN Logo" /></a>
+</div>
+
+<div id="village">
+  <a class="village-logo" href="#"><img src="<?= get_theme_file_uri('/images/VPM_Logo_RGB.png'); ?>"
+      atl="Village Logo" /></a>
+</div>
+
+<?php get_footer(); ?>
