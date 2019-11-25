1f63e68a36f6b8d666e2271cc3e6bc8873c5e005
brandon6190 <brandon6190@outlook.com>
2019-11-25 16:21:42 -0500

    -Created variables: one to hold the current pages info, and the other to hold the current pages parent's id -Checking the current pages to change navbar button color to white or black, depending on the conditions. -Moved the 'other-header' to the bottom and if it is on the homepage do not render the 'other-header' block

--- header.php
+++ header.php
@@ -7,16 +7,31 @@
   <?php wp_head(); ?>
 </head>
 
+<?php
+$parentId = wp_get_post_parent_id(get_the_ID());
+$currPage = get_queried_object();
+?>
+
 <body <?php body_class(); ?>>
   <header>
     <nav class="navbar">
       <span class="open-slide" onclick="openSlideMenu()">
         <a href="#">
+          <?php if ($currPage->post_name == 'vpm-services' or $currPage->post_parent == $parentId) : ?>
+          <svg width="30" height="30">
+            <path d="M0,5 30,5" stroke="#000" stroke-width="5" />
+            <path d="M0,14 30,14" stroke="#000" stroke-width="5" />
+            <path d="M0,23 30,23" stroke="#000" stroke-width="5" />
+          </svg>
+          <?php endif ?>
+          <?php if ($currPage->post_name == 'vseen-services' or $currPage->post_parent == $parentId) : ?>
           <svg width="30" height="30">
             <path d="M0,5 30,5" stroke="#fff" stroke-width="5" />
             <path d="M0,14 30,14" stroke="#fff" stroke-width="5" />
             <path d="M0,23 30,23" stroke="#fff" stroke-width="5" />
           </svg>
+          <?php endif ?>
+
         </a>
       </span>
     </nav>
@@ -50,3 +65,11 @@
     </div>
     <!-- Sidebar Menu: End-->
   </header>
+
+  <?php if (!is_home()) : ?>
+  <section class="section">
+    <div class="other-header">
+      <a href="<?= get_permalink($parentId); ?>" class="btn-back">&lt;</a>
+      <img src="<?= get_theme_file_uri('/images/vseen-logo.png'); ?>" />
+    </div>
+    <? endif ?>
