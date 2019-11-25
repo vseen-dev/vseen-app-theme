2a5db42e9a124731bf27d2b9e07a7001bb3efa94
brandon6190 <brandon6190@outlook.com>
2019-11-25 16:23:37 -0500

    Added the permalink for the href, so when user clicks it will bring the user to the correct services page

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <div id="vseen">
-  <a class="vseen-logo" href="#"><img src="<?= get_theme_file_uri('/images/VSEEN-LOGO-WHITE.png'); ?>"
+  <a class="vseen-logo" href="<?= get_permalink(7); ?>"><img src="<?= get_theme_file_uri('/images/vseen-logo.png'); ?>"
       alt="VSEEN Logo" /></a>
 </div>
 
 <div id="village">
-  <a class="village-logo" href="#"><img src="<?= get_theme_file_uri('/images/VPM_Logo_RGB.png'); ?>"
+  <a class="village-logo" href="<?= get_permalink(13); ?>"><img src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>"
       atl="Village Logo" /></a>
 </div>
 
