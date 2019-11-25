8f2092159b31e2ff43506367e284b5d10f83fb52
brandon6190 <brandon6190@outlook.com>
2019-11-25 16:30:08 -0500

    Created a services page for the vpm section

---
+++ page-vpm-services.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<div id="vpm-section">
+  <!-- <div class="other-header">
+    <a href="#" class="btn-back">&lt;</a>
+    <img src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>" />
+  </div> -->
+
+  <!--Section-->
+  <div class="icons-container">
+    <div class="icon-set">
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/large-format.png'); ?>" class="icon" />
+        <p class="caption">Large Print</p>
+      </div>
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/trifold.png'); ?>" class="icon" />
+        <p class="caption">
+          Trifolds
+        </p>
+      </div>
+    </div>
+    <div class="icon-set">
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/books.png'); ?>" class="icon" />
+        <p class="caption">Books</p>
+      </div>
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/postcard.png'); ?>" class="icon" />
+        <p class="caption">
+          Postcards
+        </p>
+      </div>
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/pull-up-banners.png'); ?>" class="icon" />
+        <p class="caption">Pull-up <br /> Banners</p>
+      </div>
+    </div>
+    <div class="icon-set">
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/specialty-print.png'); ?>" class="icon" />
+        <p class="caption">Specialty <br /> Print</p>
+      </div>
+      <div class="icon-box">
+        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/custom-packaging.png'); ?>" class="icon" />
+        <p class="caption">Custom <br /> Packaging</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
