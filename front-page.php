74a8d7d7b69bcbfa5c7b851a8801a3f699e56bd0
brandon6190 <brandon6190@outlook.com>
2019-12-02 14:58:21 -0500

    Moved everything from index.php to front-page.php. front-page.php is the new homepage file, while index.php is for a generic page (possibly a page for a list of all projects)

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div id="vseen">
+  <a class="vseen-logo" href="<?= site_url('/vseen-services'); ?>"><img
+      src="<?= get_theme_file_uri('/images/vseen-logo.png'); ?>" alt="VSEEN Logo" /></a>
+</div>
+
+<div id="village">
+  <a class="village-logo" href="<?= site_url('/vpm-services'); ?>"><img
+      src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>" atl="Village Logo" /></a>
+</div>
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php get_header(); ?>
-
-<div id="vseen">
-  <a class="vseen-logo" href="<?= get_permalink(7); ?>"><img src="<?= get_theme_file_uri('/images/vseen-logo.png'); ?>"
-      alt="VSEEN Logo" /></a>
-</div>
-
-<div id="village">
-  <a class="village-logo" href="<?= get_permalink(13); ?>"><img src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>"
-      atl="Village Logo" /></a>
-</div>
-
-<?php get_footer(); ?>
+This is some other page?
