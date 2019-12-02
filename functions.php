354b896594ae51296ea4294770d85c43655d9ac3
brandon6190 <brandon6190@outlook.com>
2019-12-02 15:01:53 -0500

    Added theme support for post-thumbnails

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ add_action('wp_enqueue_scripts', 'vseen_files');
 
 function vseen_features() {
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'vseen_features');
