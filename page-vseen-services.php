37b97614414d111e30ec77905eb1e0e179d56cc9
brandon6190 <brandon6190@outlook.com>
2019-12-02 15:03:05 -0500

    Added an anchor tag for each icon image, directing to respective Projects Page

--- page-vseen-services.php
+++ page-vseen-services.php
@@ -4,11 +4,13 @@
 <div class="icons-container">
   <div class="icon-set">
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/website.png'); ?>" class="icon" />
+      <a href="<?= site_url('/website'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/website.png'); ?>" class="icon" />
+      </a>
       <p class="caption">Website</p>
     </div>
     <div class="icon-box">
-      <a href="<?= get_permalink(15)?>">
+      <a href="<?= site_url('/video-production'); ?>">
         <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/video-production.png'); ?>" class="icon" />
       </a>
       <p class="caption">
@@ -19,28 +21,38 @@
   </div>
   <div class="icon-set">
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/ar.png'); ?>" class="icon" />
+      <a href="<?= site_url('/ar'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/ar.png'); ?>" class="icon" />
+      </a>
       <p class="caption">AR</p>
     </div>
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/digital-production.png'); ?>" class="icon" />
+      <a href="<?= site_url('/digital-publication'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/digital-production.png'); ?>" class="icon" />
+      </a>
       <p class="caption">
         Digital <br />
-        Production
+        Publication
       </p>
     </div>
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/photography.png'); ?>" class="icon" />
+      <a href="<?= site_url('/photography'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/photography.png'); ?>" class="icon" />
+      </a>
       <p class="caption">Photography</p>
     </div>
   </div>
   <div class="icon-set">
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/design.png'); ?>" class="icon" />
+      <a href="<?= site_url('/design'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/design.png'); ?>" class="icon" />
+      </a>
       <p class="caption">Design</p>
     </div>
     <div class="icon-box">
-      <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/animation.png'); ?>" class="icon" />
+      <a href="<?= site_url('/animation'); ?>">
+        <img src="<?= get_theme_file_uri('/images/icons/vseen-icons/animation.png'); ?>" class="icon" />
+      </a>
       <p class="caption">Animation</p>
     </div>
   </div>
