1d12512f825d9ac2e3b28fedaa9428762b0f2c85
brandon6190 <brandon6190@outlook.com>
2020-01-13 16:59:07 -0500

    Changed order of wp_enqueue_style. loading main style.css file first

--- functions.php
+++ functions.php
@@ -2,8 +2,8 @@
 
 function vseen_files() {
   wp_enqueue_script( 'vseen_main_scripts', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
-  wp_enqueue_style('normalize_css', '//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
   wp_enqueue_style('vseen_main_styles', get_stylesheet_uri(), NULL, microtime());
+  wp_enqueue_style('normalize_css', '//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'vseen_files');
