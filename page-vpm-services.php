fecec2ad0635c368bbefeb5b6b96bde2a43e3c89
brandon6190 <brandon6190@outlook.com>
2020-01-14 11:07:35 -0500

    -Added the opening section tag for vpm, together with the elements for the secondary header to display logo -Wrapped all service icons with link tags, directing user to respective projects page

--- page-vpm-services.php
+++ page-vpm-services.php
@@ -1,20 +1,25 @@
 <?php get_header(); ?>
 
-<div id="vpm-section">
-  <!-- <div class="other-header">
-    <a href="#" class="btn-back">&lt;</a>
-    <img src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>" />
-  </div> -->
+<section id="vpm-section">
 
-  <!--Section-->
+  <div class="vpm-header">
+    <a class="heading-logo" href="<?= site_url(); ?>">
+      <img src="<?= get_theme_file_uri('/images/vpm-logo.png'); ?>" />
+    </a>
+  </div>
+
   <div class="icons-container">
     <div class="icon-set">
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/large-format.png'); ?>" class="icon" />
+        <a href="<?= site_url('/large-print'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/large-format.png'); ?>" class="icon" />
+        </a>
         <p class="caption">Large Print</p>
       </div>
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/trifold.png'); ?>" class="icon" />
+        <a href="<?= site_url('/trifolds'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/trifold.png'); ?>" class="icon" />
+        </a>
         <p class="caption">
           Trifolds
         </p>
@@ -22,31 +27,41 @@
     </div>
     <div class="icon-set">
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/books.png'); ?>" class="icon" />
+        <a href="<?= site_url('/books'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/books.png'); ?>" class="icon" />
+        </a>
         <p class="caption">Books</p>
       </div>
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/postcard.png'); ?>" class="icon" />
+        <a href="<?= site_url('/postcards'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/postcard.png'); ?>" class="icon" />
+        </a>
         <p class="caption">
           Postcards
         </p>
       </div>
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/pull-up-banners.png'); ?>" class="icon" />
+        <a href="<?= site_url('/pull-up-banners'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/pull-up-banners.png'); ?>" class="icon" />
+        </a>
         <p class="caption">Pull-up <br /> Banners</p>
       </div>
     </div>
     <div class="icon-set">
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/specialty-print.png'); ?>" class="icon" />
+        <a href="<?= site_url('/specialty-print'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/specialty-print.png'); ?>" class="icon" />
+        </a>
         <p class="caption">Specialty <br /> Print</p>
       </div>
       <div class="icon-box">
-        <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/custom-packaging.png'); ?>" class="icon" />
+        <a href="<?= site_url('/custom-packaging'); ?>">
+          <img src="<?= get_theme_file_uri('/images/icons/vpm-icons/custom-packaging.png'); ?>" class="icon" />
+        </a>
         <p class="caption">Custom <br /> Packaging</p>
       </div>
     </div>
   </div>
-</div>
+</section>
 
 <?php get_footer(); ?>
